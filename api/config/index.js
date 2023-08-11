99aad268137468daaf53abf407f4023e2e56bea6
ganiefah <ganiefahmajiet3@gmail.com>
2023-08-11 16:13:15 +0200

    database connection

--- api/config/index.js
+++ api/config/index.js
@@ -1,20 +1,36 @@
-class Person{
-    #firstName = "";
-    constructor(firstName){
-        this.#firstName = firstName
-    }
-    walk(){
-        console.log(`${this.#firstName} is walking`);
-    }
-    dancing(){
-        console.log(`${this.#firstName} is dancing`);
-    }
-}
+// class Person{
+//     #firstName = "";
+//     constructor(firstName){
+//         this.#firstName = firstName
+//     }
+//     walk(){
+//         console.log(`${this.#firstName} is walking`);
+//     }
+//     dancing(){
+//         console.log(`${this.#firstName} is dancing`);
+//     }
+// }
 
-const person1 = new Person(`Ganiefah`)
-person1.walk()
+// const person1 = new Person(`Ganiefah`)
+// person1.walk()
 
-const person2 = new Person (`Laiquah`)
-person2.dancing()
+// const person2 = new Person (`Laiquah`)
+// person2.dancing()
 
 // console.log(person1.firstName);
+
+
+require("dotenv").config()
+const {createPool} = require("mysql")
+const connection = createPool({
+    host : process.env.dbHost,
+    database: process.env.dbName,
+    user: process.env.dbUser,
+    password: process.env.dbPwd,
+    multipleStatements: true,
+    connectionLimit: 30
+})
+
+module.exports = {
+    connection
+}
