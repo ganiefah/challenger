983e0b64c33dbba9e0f3f6e1b9f58d81ccc330cf
ganiefah <ganiefahmajiet3@gmail.com>
2023-08-14 12:47:13 +0200

    data

--- api/midddleware/AuthenticateUser.js
+++ api/midddleware/AuthenticateUser.js
@@ -1,1 +1,21 @@
-// Authenticate a user
+const {sign, verify} = require('jsonwebtoken')
+require("dotenv").config()
+function createToken(user) {
+    return sign({
+        emailAdd: user.emailAdd,
+        userPass: user.userPass
+    },
+    process.env.SECRET_KEY,
+    {
+        expiresIn: '1 hour'
+    }
+    )
+}
+
+// function verifyAToken(req, res, next){
+//     const token = req.headers["authorization"].
+// } 
+
+module,exports = {
+    createToken
+}
